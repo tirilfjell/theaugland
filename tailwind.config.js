faf4dedc2e2f11bb78ca826af0f6c6f6d4faaa98
Tiril <tiril@ellera.no>
2021-11-17 20:26:52 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          teal: colors.teal,
+          cyan: colors.cyan,
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
+  
