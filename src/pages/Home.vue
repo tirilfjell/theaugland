17bcf4b8f3e63647c8078636e5f3519a8e6556b8
Tiril <tiril@ellera.no>
2021-11-17 22:22:45 +0100

    new images

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -62,7 +62,7 @@
         </transition>
       </Popover>
       <main>
-        <div class="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+        <div class="pt-10 bg-green-800 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden ">
           <div class="mx-auto max-w-7xl lg:px-8">
             <div class="lg:grid lg:grid-cols-2 lg:gap-8">
               <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
@@ -98,7 +98,7 @@
               <div class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
                 <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
                   <!-- Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ -->
-                  <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="../assets/thea.png" alt="" />
+                  <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="../assets/thea4.png" alt="" />
                 </div>
               </div>
             </div>
