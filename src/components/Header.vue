f996bc9da56a7893ed7eb3062422c2387f45bb12
Tiril <tiril@ellera.no>
2022-04-14 15:20:06 +0200

    removed hire

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,7 @@
       <div class="lg:grid lg:grid-cols-2 lg:gap-4">
         <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
           <div class="lg:py-24">
-            <a href="#contact" class="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
-              <span class="ml-3 mr-3 text-sm">Available for hire</span>
-              <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-blue-500 to-green-600 rounded-full">February 2022</span>
-            </a>
+           
             <h1 class="mt-4 text-7xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-8xl lg:mt-6 xl:text-8xl">Thea Gitmark Ugland</h1>
             <h2 class="mt-10 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-5xl lg:mt-6 xl:text-5xl">
               <span class="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-indigo-600 sm:pb-5">Journalist</span>
